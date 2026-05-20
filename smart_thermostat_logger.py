d071e39408ee006c903c312118c81655457f4dca
Emmanuel John Templo <ejtemplo18@gmail.com>
2026-05-20 16:58:43 -0700

    Validating Readings
    Made the first validation reading of the users input.

--- smart_thermostat_logger.py
+++ smart_thermostat_logger.py
@@ -3,3 +3,11 @@ MAX_VALID_TEMP = 100
 
 COLD_LIMIT = 68
 WARM_LIMIT = 76
+
+# Input
+
+User_Temperature = float(input("Enter temperature readings: "))
+
+while User_Temperature <= 0:
+    print("Temperature readings need to be greater than 0.")
+    User_Temperature = float(input("Enter valid temperature readings: "))
