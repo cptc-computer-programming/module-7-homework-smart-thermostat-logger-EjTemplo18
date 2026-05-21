933e238bcad1cf839e4948051284594b3600ee55
Emmanuel John Templo <ejtemplo18@gmail.com>
2026-05-20 17:11:00 -0700

    Fixed a problem
    Fixed a problem.

--- smart_thermostat_logger.py
+++ smart_thermostat_logger.py
@@ -6,8 +6,28 @@ WARM_LIMIT = 76
 
 # Input
 
-User_Temperature = float(input("Enter temperature readings: "))
+User_Temperature = int(input("Enter temperature readings: "))
 
 while User_Temperature <= 0:
     print("Temperature readings need to be greater than 0.")
-    User_Temperature = float(input("Enter valid temperature readings: "))
+    User_Temperature = int(input("Enter valid temperature readings: "))
+
+total_temp = 0
+below_comfort = 0
+above_comfort = 0
+
+for i in range(1, User_Temperature + 1):
+    temp = int(input("Enter temperature number" + str(i) + ": "))
+
+    while temp < MIN_VALID_TEMP or temp > MAX_VALID_TEMP:
+        print("Temperature must be between", MIN_VALID_TEMP, "and", MAX_VALID_TEMP)
+        temp = int(input("Enter valid temperature number" + str(i) + ": "))
+
+    total_temp += temp
+
+    if temp < COLD_LIMIT:
+        below_comfort += 1
+    elif temp > WARM_LIMIT:
+        above_comfort += 1
+
+average_temp = total_temp / User_Temperature
